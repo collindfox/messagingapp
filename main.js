44c5bd3f4d2f4315286dbf04adb5ca8f4530d21d
CollinFox <cdfox3@illinois.edu>
2023-06-29 10:10:29 -0500

    original imports and firebase configuration

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/analytics";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDQyMIQ66TaKQ9oaAhfU0RvqT7DLXholwo",
+  authDomain: "basic-messages.firebaseapp.com",
+  projectId: "basic-messages",
+  storageBucket: "basic-messages.appspot.com",
+  messagingSenderId: "731937303281",
+  appId: "1:731937303281:web:980a927fcb319ecd4f5750",
+  measurementId: "G-YNG84L7HTS",
+});
